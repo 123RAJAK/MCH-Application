16081e7c6a6466abd0723a9f402922de61bb0267
123RAJAK <rohanrajak1998@gmail.com>
2022-10-30 18:45:21 +0530

    30/10/2022 Commit

--- MCH-Application/src/main/java/com/mch/application/controller/PatientController.java
+++ MCH-Application/src/main/java/com/mch/application/controller/PatientController.java
@@ -5,6 +5,7 @@ import java.util.concurrent.ExecutionException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +18,7 @@ import com.mch.application.serviceImplementation.patientServiceImplementation;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:4200")
 public class PatientController {
 
     @Autowired
